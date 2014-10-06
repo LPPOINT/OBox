79dbeee868ddb9a704134c6ea33c1c922b87d3e5
LPPOINT <ostrov1762@mail.ru>
2014-10-06 20:03:10 +0400

    interactive ui started (slider)

---
+++ Assets/Scripts/Map/InteractiveUI/MapItemMoverByMouse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Map.InteractiveUI
+{
+    public class MapItemMoverByMouse : MapItemsMover
+    {
+
+        private Vector2 lastMousePosition;
+        private bool lastMousePressed;
+
+
+
+        private void Update()
+        {
+
+
+            if (Input.GetMouseButton(0))
+            {
+
+                var mousePos = Input.mousePosition;
+
+                var corners = new Vector3[4];
+                UIBorder.rectTransform.GetWorldCorners(corners);
+
+                var worldRect = new Rect(corners[1].x, corners[1].y, Math.Abs(corners[2].x - corners[1].x),
+                    corners[1].y - corners[0].y);
+                worldRect.Set(worldRect.xMin, worldRect.yMin - worldRect.height, worldRect.width, worldRect.height);
+                var worldMouse = UnityEngine.Camera.main.ScreenToWorldPoint(mousePos);
+
+                if (lastMousePressed && worldRect.Contains(worldMouse))
+                {
+                    var offset = new Vector2( worldMouse.x - lastMousePosition.x,  worldMouse.y - lastMousePosition.y);
+                    Move(offset);
+                }
+
+                lastMousePressed = true;
+                lastMousePosition = worldMouse;
+
+            }
+            else
+            {
+                if(lastMousePressed) Group.NormalizePositions();
+                lastMousePressed = false;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Map/InteractiveUI/MapItemsGroup.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Levels;
+using UnityEngine;
+
+namespace Assets.Scripts.Map.InteractiveUI
+{
+    public class MapItemsGroup : LevelElement
+    {
+
+        private List<MapItem> items;
+        private Vector3 startPos;
+
+
+        private void Start()
+        {
+            items = transform.GetComponentsInChildren<MapItem>().ToList();
+            startPos = transform.position;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                NormalizePositions();
+            }
+        }
+
+
+
+
+        private void OnITweenNormalizationDone()
+        {
+            foreach (var mapItem in items)
+            {
+                mapItem.RefreshIndex();
+            }
+        }
+
+        public void NormalizePositions()
+        {
+            var probeItem = items.FirstOrDefault();
+            var closestIndex = Level.LevelMap.TileSystem.ClosestTileIndexFromWorld(probeItem.transform.position);
+            var newPosition = Level.LevelMap.TileSystem.WorldPositionFromTileIndex(closestIndex);
+            var positionsOffset = new Vector2(Mathf.Abs(probeItem.transform.position.x - newPosition.x),
+                Mathf.Abs(probeItem.transform.position.y - newPosition.y));
+
+            iTween.MoveTo(gameObject, iTween.Hash(
+                    "position", new Vector3(transform.position.x + positionsOffset.x, transform.position.y + positionsOffset.y, transform.position.z),
+                    "time", 0.3f,
+                    "oncomplete", "OnITweenNormalizationDone",
+                    "easetype", iTween.EaseType.easeInOutCirc
+                ));
+
+
+        }
+
+
+
+        public IEnumerable<MapItem> GetEdgeItems(Direction edge)
+        {
+
+
+            var minX = items.Min(item => item.Index.column);
+            var minY = items.Min(item => item.Index.row);
+
+            var maxX = items.Max(item => item.Index.column);
+            var maxY = items.Max(item => item.Index.row);
+
+            switch (edge)
+            {
+                case Direction.Top:
+                    return items.Where(item => item.Index.row == minY);
+                case Direction.Bottom:
+                    return items.Where(item => item.Index.row == maxY);
+                case Direction.Left:
+                    return items.Where(item => item.Index.column == minX);
+                case Direction.Right:
+                    return items.Where(item => item.Index.column == maxX);
+            }
+            return null;
+        }
+
+        public void Offset(Vector2 offset)
+        {
+            transform.position = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, transform.position.z);
+
+            foreach (var mapItem in items)
+            {
+                mapItem.RefreshIndex();
+            }
+        }
+
+        public override void OnLevelReset()
+        {
+            base.OnLevelReset();
+            transform.position = startPos;
+        }
+    }
+}
